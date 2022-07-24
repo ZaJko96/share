fde7eecefec159244f8211fe37e77644523b876e
ZaJko96 <65649647+ZaJko96@users.noreply.github.com>
2022-07-24 19:37:36 +0300

    Update terraform.tf

--- terraform/terraform.tf
+++ terraform/terraform.tf
@@ -1,7 +1,5 @@
 provider "aws" {
   region = "us-east-1"
-  access_key = "AKIAWTLZRDCZ5GP7TOTG"
-  secret_key = "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"
 }
 resource "aws_instance" "test" {
     ami = "ami-0cff7528ff583bf9a"
